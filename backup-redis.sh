dc34b5fbbe3502c13a1019b1f6b8a022f45ebf3b
Pirkka Jokela <pirkka.jokela@iki.fi>
2019-08-11 16:17:31 +0300

    Shell script to backup redis

--- backup-redis.sh
+++ backup-redis.sh
@@ -1,4 +1,4 @@
-!/bin/bash -e
+#! /bin/bash -e
 ssh ubuntu@pirkka.io redis-cli SAVE
 ssh ubuntu@pirkka.io "sudo mv /var/lib/redis/dump.rdb ~/redis.dump-$(date +'%Y-%m-%dT%H:%M:%S').rdb"
 ssh ubuntu@pirkka.io "sudo chown ubuntu:ubuntu ~/redis.dump*"
